81c1f5ac03c71918286d54347cd22aa852d28985
Kyan <ykyankai@gmail.com>
2021-07-04 00:08:32 +0800

    using pyproject.toml for PyPI

--- setup.py
+++ setup.py
@@ -1,34 +1,28 @@
-from setuptools import setup, find_packages
+import setuptools
 
-import consoleiotools
+with open("README.md") as f:
+    long_desc = f.read()
 with open("VERSION") as f:
     version = f.read().strip()
 
-setup(
+setuptools.setup(
     name='consoleiotools',
     version=version,
     description='Some console tools for inputs and outputs',
-    long_description='Some console tools for inputs and outputs, by Kyan',
+    long_description=long_desc,
+    long_description_content_type="text/markdown",
     url='https://github.com/kyan001/PyConsoleIOTools',
     author='Kai Yan',
     author_email='kai@kyan001.com',
     license='MIT',
     classifiers=[
-        'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'Topic :: Software Development :: User Interfaces',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3 :: Only',
     ],
     keywords='console input output tool',
-    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+    packages=setuptools.find_packages(exclude=['contrib', 'docs', 'tests']),
     py_modules=["consoleiotools"],
     install_requires=['colorama'],
     extras_require={},
