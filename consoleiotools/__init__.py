73f0b38d37ef860bde426d2604de78114c7dc52d
Kyan <ykyankai@gmail.com>
2022-12-07 18:40:27 +0800

    move to consoleiotools folder

--- consoleiotools/__init__.py
+++ consoleiotools/__init__.py
@@ -7,8 +7,9 @@ import rich.text
 import rich.progress
 import rich.traceback
 import rich.markdown
+import rich.box
 
-__version__ = "3.4.3"
+__version__ = "4.0.0"
 theme = rich.theme.Theme({
     "echo": "on black",
     "echo-bar": "on black",
@@ -25,14 +26,21 @@ theme = rich.theme.Theme({
     "ask": "yellow",
     "ask-bar": "yellow",
     "ask-pre": "dim yellow",
-    "blur": "dim bright_white",
-    "blur-bar": "dim white",
-    "blur-pre": "dim white",
-    "title": "bright_cyan",
-    "pause": "yellow",
+    "muted": "dim bright_white",
+    "muted-bar": "dim white",
+    "muted-pre": "dim white",
+    "title": "bright_yellow",
+    "pause": "magenta",
+    "choice-i": "yellow",
+    "choice-cmd": "bright_yellow underline",
 })
+__ascii__ = False
 console = rich.console.Console(theme=theme)  # main output printer
-rich.traceback.install(show_locals=True)
+rich.traceback.install()
+
+
+def debug(show_locals: bool = True) -> None:
+    rich.traceback.install(show_locals=show_locals)
 
 
 def as_session(name_or_func):  # decorator
@@ -44,8 +52,8 @@ def as_session(name_or_func):  # decorator
     if callable(name_or_func):  # no name provided
         func = name_or_func
         name = func.__name__
-        name = "".join([(' ' + x) if x.isupper() else x for x in name])
-        name = name.replace('_', ' ')
+        name = "".join([(" " + x) if x.isupper() else x for x in name])
+        name = name.replace("_", " ")
         return as_session(name)(func)  # deco(func) -> deco(name)(func)
     else:
         name = name_or_func
@@ -67,15 +75,15 @@ def start():
 
 
 def end():
-    console.print("╰")
+    console.print("`" if __ascii__ else "╰")
 
 
 def br(count=1):
     """print 1 to N blank lines"""
-    print('\n' * (count - 1))
+    print("\n" * (count - 1))
 
 
-def echo(*args, pre: str = "", bar: str = "│", style: str = "echo", **options):
+def echo(*args, pre: str = "", bar: str = "|" if __ascii__ else "│", style: str = "echo", **options):
     txt = rich.text.Text()
     if bar:
         txt.append(f"{bar}", style=f"{style}-bar" if f"{style}-bar" in theme.styles else style)
@@ -89,7 +97,7 @@ def echo(*args, pre: str = "", bar: str = "│", style: str = "echo", **options)
 
 def title(*args, **options):
     """print something like a title"""
-    console.print(rich.panel.Panel((" ".join([f"{arg}" for arg in args])).upper().strip(), highlight=True, expand=False), **options)
+    console.print(rich.panel.Panel((" ".join([f"{arg}" for arg in args])).upper().strip(), highlight=True, expand=False, box=rich.box.ASCII if __ascii__ else rich.box.ROUNDED), **options)
 
 
 def ask(*args, **options):
@@ -108,8 +116,8 @@ def err(*args, **options):
     return echo(*args, pre="error", style="err", **options)
 
 
-def dim(*args, **options):
-    return echo(*args, style="blur", **options)
+def mute(*args, **options):
+    return echo(*args, style="muted", **options)
 
 
 def print(*args, **options):
@@ -120,15 +128,30 @@ def markdown(*args, **options):
     console.print(rich.markdown.Markdown(" ".join([f"{arg}" for arg in args])))
 
 
+def rule(title: str, *args, **options):
+    console.rule(title, *args, characters="-" if __ascii__ else "─", **options)
+
+
 def panel(txt, title="", subtitle="", expand=True, highlight=True, style="", **options):
-    return console.print(rich.panel.Panel(txt, title=title, subtitle=subtitle, highlight=highlight, expand=expand, style=style, border_style=f"{style}-pre" if f"{style}-pre" in theme.styles else "", **options))
+    return console.print(
+        rich.panel.Panel(
+            txt,
+            title=title,
+            subtitle=subtitle,
+            highlight=highlight,
+            expand=expand,
+            style=style,
+            border_style=f"{style}-pre" if f"{style}-pre" in theme.styles else "",
+            box=rich.box.ASCII if __ascii__ else rich.box.ROUNDED,
+            **options
+        )
+    )
 
 
 def pause(msg="Press [Enter] to Continue..."):
     """press to continue"""
-    br()
-    echo(f"{rich.markup.escape(msg)}", bar="", style="pause", end="")
-    return input()
+    with console.status(f"[pause]{rich.markup.escape(msg)}", spinner="point"):
+        return input()
 
 
 def bye(msg=""):
@@ -150,11 +173,13 @@ def get_choice(choices, exitable: bool = False):
         exitable: bool. Does `exit` is an option for user to select.
     """
     EXIT_WORD = "exit" if "0" in choices else "0"
+    DECO_WORD = "[dim]{}[/]".format("--" if __ascii__ else "──")
+    BAR_WORD = "|" if __ascii__ else "│"
     fill = max(len(EXIT_WORD), 2)
     for index, item in enumerate(choices, start=1):
-        console.print(f"  [yellow]{index:>{fill}})[/] [white]{item}[/]")
+        console.print(f"{BAR_WORD} [choice-i]{index:>{fill}}[/][dim])[/] [white]{item}[/]")
     if exitable:
-        console.print(f"  [yellow]{EXIT_WORD:>{fill}}) ** [b]EXIT[/] **")
+        console.print(f"{BAR_WORD} [choice-i]{EXIT_WORD:>{fill}}[/][dim])[/] {DECO_WORD} [choice-cmd]EXIT[/] {DECO_WORD}")
     user_choice = get_input().strip()
     if exitable and user_choice == EXIT_WORD:
         return None
@@ -179,18 +204,22 @@ def get_choices(choices, allable: bool = False, exitable: bool = False) -> list:
     EXIT_WORD = "exit" if "0" in choices else "0"
     DONE_WORD = "done" if "0" in choices else "0"
     ALL_WORD = "all" if "a" in choices else "a"
+    DECO_WORD = "[dim]{}[/]".format("--" if __ascii__ else "──")
+    BAR_WORD = "|" if __ascii__ else "│"
+    BRACKET_WORD = "[cyan]" + rich.console.escape("[") + "{}][/]"
     fill = max(len(EXIT_WORD), len(DONE_WORD), len(ALL_WORD), 2)
     user_choices = []
     while True:
         if allable:
-            console.print(f"  [yellow]{ALL_WORD:>{fill}}) ** [b]ALL[/] **")
+            console.print(f"{BAR_WORD} [choice-i]{ALL_WORD:>{fill}}[/][dim])[/] {DECO_WORD} [choice-cmd]ALL[/] {DECO_WORD}")
         for index, item in enumerate(choices, start=1):
-            mark = r"\[[green]+[/]]" if item in user_choices else rich.markup.escape("[ ]")  # item is selected or not
-            console.print(f"  [yellow]{index:>{fill}})[/] {mark} [white]{item}")
+
+            mark = BRACKET_WORD.format(f"[bright_green]{'+' if __ascii__ else '✓'}[/]") if item in user_choices else BRACKET_WORD.format(" ")  # item is selected or not
+            console.print(f"{BAR_WORD} [choice-i]{index:>{fill}}[/][dim])[/] {mark} [white]{item}")
         if user_choices:  # user selections > 0
-            console.print(f"  [yellow]{DONE_WORD:>{fill}}) ** [b]DONE[/] **")
+            console.print(f"{BAR_WORD} [choice-i]{DONE_WORD:>{fill}}[/][dim])[/] {DECO_WORD} [choice-cmd]DONE[/] {DECO_WORD}")
         elif exitable:  # no user selection, but exitable is on.
-            console.print(f"  [yellow]{EXIT_WORD:>{fill}}) ** [b]EXIT[/] **")
+            console.print(f"{BAR_WORD} [choice-i]{EXIT_WORD:>{fill}}[/][dim])[/] {DECO_WORD} [choice-cmd]EXIT[/] {DECO_WORD}")
         user_choice = get_input().strip()
         if (user_choice == DONE_WORD or user_choice == EXIT_WORD):
             if exitable or len(user_choices) > 0:  # keep looping when not exitable and no user choices.
@@ -210,8 +239,7 @@ def get_choices(choices, allable: bool = False, exitable: bool = False) -> list:
 
 
 def track(iterable, desc="", unit="", *args, **options):
-    with rich.progress.Progress("│", rich.progress.SpinnerColumn(), *rich.progress.Progress.get_default_columns(),
-                                "·", rich.progress.MofNCompleteColumn(), unit) as progress:
+    with rich.progress.Progress("|" if __ascii__ else "│", rich.progress.SpinnerColumn(), *rich.progress.Progress.get_default_columns(), "·", rich.progress.MofNCompleteColumn(), unit, *args, **options) as progress:
         task = progress.add_task(desc, total=None if not iterable or not len(iterable) else len(iterable))
         while not progress.finished:
             for item in iterable:
@@ -220,17 +248,17 @@ def track(iterable, desc="", unit="", *args, **options):
 
 
 def read_file(path: str, with_encoding: bool = False, **kwargs):
-    for enc in ("utf-8", 'gbk', 'cp1252', 'windows-1252', 'latin-1'):
+    for enc in ("utf-8", "gbk", "cp1252", "windows-1252", "latin-1"):
         try:
-            with open(path, mode='r', encoding=enc, **kwargs) as f:
+            with open(path, mode="r", encoding=enc, **kwargs) as f:
                 return (f.read(), enc) if with_encoding else f.read()
         except UnicodeDecodeError:
             pass
 
 
 def write_file(path: str, content: str, overwrite: bool = False, **kwargs):
-    mode = 'w' if overwrite else 'a'
-    with open(path, mode=mode, encoding='utf-8', **kwargs) as fl:
+    mode = "w" if overwrite else "a"
+    with open(path, mode=mode, encoding="utf-8", **kwargs) as fl:
         return fl.write(content)
 
 
